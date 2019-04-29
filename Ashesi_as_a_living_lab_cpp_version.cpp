59d90f5bb7d220b6e5cca5df78f42e3aaf89d893
denverchikokonya <46723468+denverchikokonya@users.noreply.github.com>
2019-04-29 10:10:22 +0000

    Update Ashesi_as_a_living_lab_cpp_version.cpp

--- Ashesi_as_a_living_lab_cpp_version.cpp
+++ Ashesi_as_a_living_lab_cpp_version.cpp
@@ -1,4 +1,8 @@
 //
+// Created by Denver Chikokonya on 4/29/2019.
+//
+
+//
 // Created by Denver Chikokonya on 4/27/2019.
 //
 
@@ -22,7 +26,7 @@ int id =1;
 int itemsScanned=0;
 ofstream myfile;
 
-
+//create account
 void createAccount() {
     string name;
     double amount = 0;
@@ -34,7 +38,7 @@ void createAccount() {
 
 
 
-
+//writing to a file.
     myfile.open("users.txt");
     if (myfile.is_open())
     {
@@ -45,7 +49,7 @@ void createAccount() {
             string name = get<0>(e);
             double amount = get<1>(e);
             int num = get<2>(e);
-            myfile <<"ID:"<<key<<" "<<"NAME:"<<name<<" "<<"BALANCE:"<<amount<<" "<<"ITEMS SCANNED:"<<num<<"\n";
+            myfile <<"ID:"<<key<<" "<<"NAME:"<<name<<" "<<"BALANCE in GHc:"<<amount<<" "<<"ITEMS SCANNED:"<<num<<"\n";
         }
         myfile.close();
         cout<<"Your account has been created! and your ID is: "<<id <<"\n";
@@ -55,62 +59,66 @@ void createAccount() {
     else cout << "Unable to open file";
 
 }
-
+//scanning an item
 void scan(){
     int bar;
     string name;
     int id;
-    //ofstream myfile;
-    //ifstream myfile;
 
-    cout<<" Please enter your id: ";
-    cin>>id;
+    if (count<4) {
+        cout << " Please enter your id: ";
+        cin >> id;
 
-    cout<<" Please enter the barcode: ";
-    cin>>bar;
+        cout << " Please enter the barcode: ";
+        cin >> bar;
+//crediting the 20 pesewas
+        tuple<string, double, int> details = customers[id];
+        get<1>(details) = get<1>(details) + 0.2;
+        get<2>(details) = get<2>(details) + 1;
+        customers[id] = details;
+        users[name] = users[name] + 0.2;
 
-    tuple<string,double,int> details = customers[id];
-    get<1>(details)=  get<1>(details)+0.2;
-    get<2>(details)=  get<2>(details)+1;
-    customers[id]=details;
-    users[name] = users[name]+0.2;
+        for (int x:barcodes) {
+            if (x == bar) {
+                cout << "Item has already been scanned \n";
+                break;
+            }
+        }
+        barcodes.push_back(bar);
 
-    for (int x:barcodes){
-        if (x == bar){
-            cout<< "Item has already been scanned \n";
-            break;
+//writing to a file
+        myfile.open("users.txt");
+        if (myfile.is_open()) {
+
+            for (const auto &p : customers) {
+                const auto &d = p.first;
+                const auto &e = p.second;
+                int key = d;
+                string name = get<0>(e);
+                double amount = get<1>(e);
+                int num = get<2>(e);
+                myfile << "ID:" << key << " " << "NAME:" << name << " " << "BALANCE in GHc:" << amount << " "
+                       << "ITEMS SCANNED:" << num << "\n";
+            }
+            myfile.close();
+
+        } else {
+            cout << "Unable to open file";
         }
     }
-    barcodes.push_back(bar);
 
-
-    myfile.open("users.txt");
-    if (myfile.is_open()){
-
-        for (const auto& p : customers){
-            const auto& d = p.first;
-            const auto& e = p.second;
-            int key = d;
-            string name=get<0>(e);
-            double amount=get<1>(e);
-            int num=get<2>(e);
-            myfile <<"ID:"<<key<<" "<<"NAME:"<<name<<" "<<"BALANCE:"<<amount<<" "<<"ITEMS SCANNED:"<<num<<"\n";
-        }
-        myfile.close();
-
+    else{
+        cout<<"Bin is full";
     }
-
-    else {
-        cout << "Unable to open file";}
 
 
 
 }
-
+//payment of other services
 void pay() {
     string name;
     string itemBought;
-    int price;
+    double price;
     int id;
 
     cout << " Please enter your ID";
@@ -122,9 +130,9 @@ void pay() {
     cout << " Please enter its price";
     cin >> price;
 
-    if (get<2>(customers[id]) >= price) {
+    if (get<1>(customers[id]) >= price) {
         tuple<string, double, int> details = customers[id];
-        get<2>(details) = get<2>(details) - price;
+        get<1>(details) = get<1>(details) - price; // deduct from balance
         customers[id] = details;
 
 
@@ -138,7 +146,7 @@ void pay() {
                 string name=get<0>(e);
                 double amount=get<1>(e);
                 int num=get<2>(e);
-                myfile <<"ID:"<<key<<" "<<"NAME:"<<name<<" "<<"BALANCE:"<<amount<<" "<<"ITEMS SCANNED:"<<num<<"\n";
+                myfile <<"ID:"<<key<<" "<<"NAME:"<<name<<" "<<"BALANCE in GHc:"<<amount<<" "<<"ITEMS SCANNED:"<<num<<"\n";
             }
             myfile.close();
 
@@ -161,7 +169,7 @@ void viewCustomers(){
 
 
     cout<<"\n List of users and their current balances: \n";
-
+//opening text file and displaying the users
     string line;
     ifstream myfile ("users.txt");
     if (myfile.is_open())
@@ -187,30 +195,44 @@ void viewCustomers(){
 
 }
 
+//Checking Bin Status
+void checkBin(){
+    if (count<4 ){
+        cout<<"The bin is full.";
+    }
+    else{
+        cout<<"There is still space in the bin" ;
+    }
+}
+
+//The guardian of the future
 void award(){
 
     int max =get<2>(customers[1]);
-    string winner;
+    string winner=get<0>(customers[1]);
+
     for (const auto& p : customers){
         const auto& d = p.first;
         const auto& e = p.second;
-        if (get<2>(e)>max){
-            max=get<2>(e);
+        int compare=get<2>(e);
+        if (compare>max){
+            max=compare;
             winner=get<0>(e);
+
 
         }
     }
     cout<<"The guardian of the future is: "<<winner<<"! \n";
 }
 
-
+//menu
 void menu() {
     int choice;
     while (choice != 3) {
         cout << "\n Welcome to Ashesi as a Living Lab Recycling system! \n ";
         cout << "Please select from options below: \n";
         cout << "\n";
-        cout << "1. User Account \n 2. Stakeholder \n3. Exit \n";
+        cout << "1. User Account \n2. Stakeholder \n3. Exit \n";
         cin >> choice;
         while (choice < 1 || choice > 3) {
             cout << "Invalid choice!";
@@ -218,31 +240,33 @@ void menu() {
         }
         if (choice == 1) {
 
-            string option;
+            int option;
             cout
-                    << "Please choose from the menu below(select c for Account creation, s for scan, b to purchase an item: \n";
-            cout << "c. Create account \n s. Scan item \n b. Buy item";
+                    << "Please choose from the menu below(select 1 for Account creation, 2 for scan, 3 to purchase an item: \n";
+            cout << "1. Create account \n2. Scan item \n3. Buy item";
             cin >> option;
-            if (option == "c") {
+            if (option == 1) {
                 createAccount();
-            } else if (option == "s") {
+            } else if (option == 2) {
                 scan();
-            } else if (option == "b") {
+            } else if (option == 3) {
                 pay();
+            } else{
+                cout<<"Invalid Option!";
             }
 
 
         } else if (choice == 2) {
-            string option;
-            cout<< "\n Please choose from the menu below(select v to View Users, g to View and Award Guardian of the future , c to check the bins' status: \n";
-            cout << "v. View users \n g. View and Award Guardian of the future \n s. check the bins' status";
+            int option;
+            cout<< "\n Please choose from the menu below(select 1 to View Users, 2 to View and Award Guardian of the future , 3 to check the bins' status: \n";
+            cout << "1. View users \n2. View and Award Guardian of the future \n3. check the bins' status";
             cin >> option;
-            if (option == "v") {
+            if (option == 1) {
                 viewCustomers();
-            } else if (option == "g") {
+            } else if (option == 2) {
                 award();
-            } else if (option == "s") {
-                cout << "\n nothing for now";
+            } else if (option == 3) {
+                checkBin();
             }
 
 
@@ -252,5 +276,5 @@ void menu() {
 
 }
 int main(){
-   menu();
+    menu();
 }
